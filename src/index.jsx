1bc38e7c6002387eed356faf0b8b9124b4b07a2b
yoskeoka <yoske95@gmail.com>
2018-01-10 03:19:59 +0900

    connect to the server

--- src/index.jsx
+++ src/index.jsx
@@ -3,22 +3,15 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { AppContainer } from "react-hot-loader";
+import io from "socket.io-client";
 
 import reducer from "./reducer";
 import Routes from "./Routes";
 
 const store = createStore(reducer);
 
-// test data
-store.dispatch({
-    type: "SET_STATE",
-    state: {
-        vote: {
-            pair: ["Sunshine", "28 Days Later"],
-            tally: { Sunshine: 2 },
-        },
-    },
-});
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on("state", state => store.dispatch({ type: "SET_STATE", state }));
 
 const render = Component => {
     ReactDOM.render(
