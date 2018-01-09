91ded82a6cfcd5719346aaa71f5a639d19cf132e
yoskeoka <yoske95@gmail.com>
2018-01-10 02:20:15 +0900

    remove App wrapping

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,23 +5,11 @@ import Voting from "./containers/Voting";
 import Results from "./containers/Results";
 import NotFound from "./NotFound";
 
-const AppVoting = () => (
-    <App>
-        <Voting />
-    </App>
-);
-
-const AppResults = () => (
-    <App>
-        <Results />
-    </App>
-);
-
 const Routes = () => (
     <Router>
         <Switch>
-            <Route exact path="/" component={AppVoting} />
-            <Route path="/results" component={AppResults} />
+            <Route exact path="/" component={Voting} />
+            <Route path="/results" component={Results} />
             <Route component={NotFound} />
         </Switch>
     </Router>
